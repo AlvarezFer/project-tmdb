78c5c93a8c157ab61b09f7965c15eb6b46181a2e
FerAlvarez33 <fernicomeres@gmail.com>
2023-01-30 03:08:11 -0300

    Se agrego carrusel

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-
-import Navbar from "./commons/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Movies from "./components/Movies";
 import SelectMovie from "./components/SelectMovie";
 import AuthContext from "./context/AuthContext";
+import NotFound from "./components/NotFound";
+
+import DarkVariantExample from "./components/Carousel";
+
+import "././global.css";
 
 const App = () => {
   return (
     <>
       <AuthContext>
         <Routes>
-          <Route path="/" element={<Navbar />}></Route>
+          <Route path="/" element={<DarkVariantExample />}></Route>
 
           <Route path="/movies" element={<Movies />}></Route>
           <Route path="/movies/:movieId" element={<SelectMovie />}></Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthContext>
     </>

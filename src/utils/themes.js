cdbac248c80647e2c9b480622ff1b4f4640ade68
AlvarezFer <fernicomeres@gmail.com>
2023-03-17 16:24:26 -0300

    Se cambio color

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,5 +1,5 @@
 const themes = {
-  light: { sintax: "#555", ui: "#ddd", bg: "#fff" },
+  light: { sintax: "#000", ui: "#ddd", bg: "#fff" },
   dark: { sintax: "#ddd", ui: "#000", bg: "#555" },
 };
 
